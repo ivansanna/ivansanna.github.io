6a6dc8719e8e4d366b5d7c35912f987db6f67ce8
Ivan Sanna <ivansanna@Ivans-MacBook-Pro.local>
2024-09-10 19:06:12 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const ctaButton = document.querySelector('.cta-button');
+    ctaButton.addEventListener('click', function() {
+        alert('More information coming soon!');
+    });
+});
